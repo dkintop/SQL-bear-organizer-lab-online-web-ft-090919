f63b13f4fd9ee110010ba7d06cbc13198de2e552
Doug Kintop <dkintop@hotmail.com>
2019-10-07 19:14:45 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,8 +15,6 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 
-#SELECT * FROM cats ORDER BY age DESC LIMIT 1;
-
 
 def select_youngest_bear_and_returns_name_and_age
    "SELECT name, age FROM bears ORDER BY age LIMIT 1"
